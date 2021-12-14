0b89e51692ae19bf71ff69cbb830927b95b783bf
rocknegi <rocknegi53@gmail.com>
2021-12-14 23:21:56 +0100

    Add swipeable cards and swipe functionality Gestures + buttons to swipe Update UI of home screen

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import { useNavigation } from "@react-navigation/native";
-import React, { useLayoutEffect } from "react";
+import React, { useLayoutEffect, useRef } from "react";
 import {
   View,
   Text,
@@ -7,6 +7,7 @@ import {
   SafeAreaView,
   TouchableOpacity,
   Image,
+  StyleSheet,
 } from "react-native";
 import tw from "tailwind-rn";
 import { AntDesign, Entypo, Ionicons } from "@expo/vector-icons";
@@ -20,28 +21,35 @@ const HomeScreen = () => {
   type navigationType = NativeStackNavigationProp<RootStackParamList, "Chat">;
   const navigation = useNavigation<navigationType>();
   const { logout, user }: useAuthTypes = useAuth();
+  const swipeRef = useRef<any>(null);
 
   const DUMMY_DATA = [
     {
       id: 1,
-      firstname: "Rohit",
-      lastName: "Negi",
-      occupation: "SDE II",
+      firstname: "Taylor",
+      lastName: "Swift",
+      job: "Singer",
       age: 24,
+      photoURL:
+        "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTU1NDc3MTEyODE0MzE0NTcy/taylor-swift-attends-the-2016-vanity-fair-oscar-party-hosted-by-graydon-carter-at-wallis-annenberg-center-for-the-performing-arts-on-february-28-2016-in-beverly-hills-california-photo-by-anthony-harve.jpg",
     },
     {
       id: 2,
-      firstname: "Ro",
-      lastName: "Negi",
-      occupation: "SDE II",
+      firstname: "Olivia ",
+      lastName: "Rodrigo",
+      job: "Singer",
       age: 24,
+      photoURL:
+        "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTg0ODYwOTg5MDMwNjcxNDgw/gettyimages-1184956791.jpg",
     },
     {
       id: 3,
-      firstname: "rt",
-      lastName: "Negi",
-      occupation: "SDE II",
+      firstname: "Dua ",
+      lastName: "Lipa",
+      job: "Singer",
       age: 24,
+      photoURL:
+        "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTg1NjUxMjM2MTY5NTkwMTY0/gettyimages-1356296274.jpg",
     },
   ];
 
@@ -74,17 +82,97 @@ const HomeScreen = () => {
       {/* Cards */}
       <View style={tw("flex-1 -mt-6")}>
         <Swiper
+          ref={swipeRef}
+          stackSize={5}
+          cardIndex={0}
+          verticalSwipe={false}
+          animateCardOpacity
           containerStyle={{ backgroundColor: "transparent" }}
           cards={DUMMY_DATA}
+          onSwipedLeft={() => console.log("swipe pass")}
+          onSwipedRight={() => console.log("swipe match")}
+          backgroundColor="#4FD0E9"
+          overlayLabels={{
+            left: {
+              title: "NOPE",
+              style: {
+                label: {
+                  textAlign: "right",
+                  color: "red",
+                },
+              },
+            },
+            right: {
+              title: "MATCH",
+              style: {
+                label: {
+                  color: "#4DED30",
+                },
+              },
+            },
+          }}
           renderCard={(card) => (
-            <View key={card.id} style={tw("bg-white h-3/4 rounded-xl")}>
-              <Text>{card.firstname}</Text>
+            <View
+              key={card.id}
+              style={tw("relative bg-white h-3/4 rounded-xl")}
+            >
+              <Image
+                style={tw("h-full w-full rounded-xl")}
+                source={{ uri: card.photoURL }}
+              />
+              <View
+                style={[
+                  tw(
+                    "absolute bottom-0 flex-row justify-between items-center bg-white w-full h-20 px-6 py-2 rounded-b-xl"
+                  ),
+                  styles.cardShadow,
+                ]}
+              >
+                <View>
+                  <Text style={tw("text-xl font-bold")}>
+                    {card.firstname} {card.lastName}
+                  </Text>
+                  <Text>{card.job}</Text>
+                </View>
+                <Text style={tw("text-2xl font-bold")}>{card.age}</Text>
+              </View>
             </View>
           )}
         />
+      </View>
+      <View style={tw("flex flex-row justify-evenly mb-2")}>
+        <TouchableOpacity
+          onPress={() => swipeRef?.current?.swipeLeft()}
+          style={tw(
+            "items-center justify-center rounded-full w-16 h-16 bg-red-200"
+          )}
+        >
+          <Entypo name="cross" size={24} color="red" />
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => swipeRef?.current?.swipeRight()}
+          style={tw(
+            "items-center justify-center rounded-full w-16 h-16 bg-green-200"
+          )}
+        >
+          <Entypo name="heart" size={24} color="green" />
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
 };
 
 export default HomeScreen;
+
+const styles = StyleSheet.create({
+  cardShadow: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+    elevation: 2.1,
+  },
+});
